992fde298d93d0358ac5054e9f0fce1fe9ece669
Nicolas Voyez <n.voyez@criteo.com>
2020-12-13 14:08:39 +0100

    When the winged maths arrive !

---
+++ AdventOfCode2020/Solvers/SolverDay12.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2020.Solvers
+{
+    class SolverDay12 : ISolver
+    {
+        enum Order
+        {
+            West = 0,
+            North = 1,
+            East = 2,
+            South = 3,
+            Forward,
+            RotateLeft,
+            RotateRight
+        }
+
+        struct NavigationAction
+        {
+            public Order Order { get; }
+            public int Value { get; }
+
+            public NavigationAction(string order):this(GetOrder(order[0]), int.Parse(order.Substring(1)))
+            {
+
+            }
+
+            private static Order GetOrder(char c)
+            {
+                switch (c)
+                {
+                    case 'E': return Order.East;
+                    case 'W': return Order.West;
+                    case 'N': return Order.North;
+                    case 'S': return Order.South;
+                    case 'L': return Order.RotateLeft;
+                    case 'R': return Order.RotateRight;
+                    case 'F': return Order.Forward;
+                }
+                throw new NotImplementedException();
+            }
+
+            public NavigationAction(Order o, int value)
+            {
+                Order = o;
+                Value = value;
+            }
+        }
+
+        class Boat
+        {
+            public int East { get; set; }
+            public int North { get; set; }
+
+            public int WayPointEast { get; set; } = 10;
+            public int WayPointNorth { get; set; } = 1;
+            public Order FacingDirection { get; private set; } = Order.East;
+
+            public Boat()
+            {
+            }
+
+            public void MoveExercice1(NavigationAction action)
+            {
+                switch (action.Order)
+                {
+                    case Order.West:
+                        East -= action.Value;
+                        break;
+                    case Order.North:
+                        North += action.Value;
+                        break;
+                    case Order.East:
+                        East += action.Value;
+                        break;
+                    case Order.South:
+                        North -= action.Value;
+                        break;
+                    case Order.Forward:
+                        MoveExercice1(new NavigationAction(FacingDirection, action.Value));
+                        break;
+                    case Order.RotateRight:
+                        FacingDirection = (Order) (((int) FacingDirection + action.Value / 90) % 4);
+                        break;
+                    case Order.RotateLeft:
+                        var val = ((int) FacingDirection - action.Value / 90);
+                        if (val < 0)
+                            val += 4;
+                        FacingDirection = (Order)(val % 4);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            public void MoveExercice2(NavigationAction action)
+            {
+                switch (action.Order)
+                {
+                    case Order.West:
+                        WayPointEast -= action.Value;
+                        break;
+                    case Order.North:
+                        WayPointNorth += action.Value;
+                        break;
+                    case Order.East:
+                        WayPointEast += action.Value;
+                        break;
+                    case Order.South:
+                        WayPointNorth -= action.Value;
+                        break;
+                    case Order.Forward:
+                        East += WayPointEast * action.Value;
+                        North += WayPointNorth * action.Value;
+                        break;
+                    case Order.RotateRight:
+                        var tempNorth = WayPointNorth;
+                        var tempEast = WayPointEast;
+                        if (action.Value == 90)
+                        {
+                            WayPointEast = tempNorth;
+                            WayPointNorth = -tempEast;
+                        }
+                        else if (action.Value == 180)
+                        {
+                            WayPointEast = -tempEast;
+                            WayPointNorth = -tempNorth;
+                        }
+                        else
+                        {
+                            WayPointEast = -tempNorth;
+                            WayPointNorth = tempEast;
+                        }
+                        break;
+                    case Order.RotateLeft:
+                        var tn = WayPointNorth;
+                        var te = WayPointEast;
+                        if (action.Value == 90)
+                        {
+                            WayPointEast = -tn;
+                            WayPointNorth = te;
+                        }
+                        else if (action.Value == 180)
+                        {
+                            WayPointEast = -te;
+                            WayPointNorth = -tn;
+                        }
+                        else
+                        {
+                            WayPointEast = tn;
+                            WayPointNorth = -te;
+                        }
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        private List<NavigationAction> _navigationActions = new List<NavigationAction>();
+
+        public void InitInput(string content)
+        {
+            var splitContent = content.Split(new string[] {"\r\n"}, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var currentLine in splitContent)
+            {
+                _navigationActions.Add(new NavigationAction(currentLine));
+            }
+        }
+
+        public string SolveFirstProblem()
+        {
+            var boat = new Boat();
+            foreach (var navigationAction in _navigationActions)
+            {
+                boat.MoveExercice1(navigationAction);
+            }
+
+            return (Math.Abs(boat.East) + Math.Abs(boat.North)).ToString();
+        }
+
+        public string SolveSecondProblem(string firstProblemSolution)
+        {
+            var boat = new Boat();
+            foreach (var navigationAction in _navigationActions)
+            {
+                boat.MoveExercice2(navigationAction);
+            }
+
+            return (Math.Abs(boat.East) + Math.Abs(boat.North)).ToString();
+        }
+
+        public bool Question2CodeIsDone { get; } = true;
+    }
+}
---
+++ AdventOfCode2020/Solvers/SolverDay13.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace AdventOfCode2020.Solvers
+{
+    class SolverDay13 : ISolver
+    {
+        private int _earlierTimestamp;
+        private readonly List<int> _exercise1BusLines = new List<int>();
+        private readonly Dictionary<int,int> _exercise2BusLinesDelays = new Dictionary<int,int>();
+        public void InitInput(string content)
+        {
+            var splitContent = content.Split(new string[] {"\r\n"}, StringSplitOptions.RemoveEmptyEntries);
+
+            _earlierTimestamp = int.Parse(splitContent[0]);
+            var busLines = splitContent[1].Split(new string[] {","}, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < busLines.Length; i++ )
+            {
+                var time = busLines[i];
+                if (time == "x")
+                    continue;
+                var busLine = int.Parse(time);
+                _exercise1BusLines.Add(busLine);
+                _exercise2BusLinesDelays[busLine] = i;
+            }
+
+        }
+
+        public string SolveFirstProblem()
+        {
+            var bestDiff = int.MaxValue;
+            var bestLine = -1;
+            foreach (var busLine in _exercise1BusLines)
+            {
+                var diff = (((_earlierTimestamp / busLine) * (busLine)) + busLine) - _earlierTimestamp;
+                //var diff = _earlierTimestamp % busLine;
+                if (diff < bestDiff)
+                {
+                    bestDiff = diff;
+                    bestLine = busLine;
+                }
+            }
+
+            return (bestDiff * bestLine).ToString();
+        }
+
+        public string SolveSecondProblem(string firstProblemSolution)
+        {
+            // x % 7 == 0
+            // x % 13 == (13-1)
+            // x % 59 == (59-4)
+            // ....
+
+            double n = 1;
+            foreach (var busLine in _exercise2BusLinesDelays.Keys)
+            {
+                n *= busLine;
+            }
+
+            BigInteger oneSolution = 0;
+            foreach (var kvp in _exercise2BusLinesDelays)
+            {
+                double ni = kvp.Key;
+                double invni = n / ni;
+
+                var factor = -1;
+                for (int j = 1; j <= ni; j++)
+                {
+                    if (Math.Abs((j * invni) % ni - 1) < double.Epsilon)
+                    {
+                        factor = j;
+                        break;
+                    }
+                }
+                if (factor == -1)
+                    throw new Exception();
+
+                BigInteger ei = new BigInteger(factor * invni);
+                oneSolution +=  ei * new BigInteger(((ni - kvp.Value) % ni));
+            }
+
+            var res = oneSolution % new BigInteger(n);
+
+            return res.ToString();
+            /* non working brute force
+            var maxBus = _exercise1BusLines.Max();
+            var maxBusDelay = _exercise2BusLinesDelays[maxBus];
+
+            double round = 35155718987;
+            while (true)
+            {
+                double timestamp = maxBus * round - maxBusDelay;
+                if (TimestampIsValid(timestamp))
+                    return timestamp.ToString();
+                round++;
+            }*/
+        }
+
+        private bool TimestampIsValid(in double timestamp)
+        {
+            foreach (var kvp in _exercise2BusLinesDelays)
+            {
+                if (((timestamp + kvp.Value) % kvp.Key) != 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        public bool Question2CodeIsDone { get; } = true;
+    }
+}
