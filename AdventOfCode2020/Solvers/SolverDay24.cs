7391070f08dd4faf482a1d161c4e6e68d52be94c
Nicolas Voyez <n.voyez@criteo.com>
2020-12-25 23:24:55 +0100

    Santa passed, time to end the puzzles :)

---
+++ AdventOfCode2020/Solvers/SolverDay24.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2020.Solvers
+{
+    class SolverDay24 : ISolver
+    {
+        class Hex
+        {
+            public int X { get; }
+            public int Y { get; }
+            public bool IsBlack { get; set; }
+
+            /// <summary>
+            /// Note : X and Y are multiples of 10
+            /// </summary>
+            public Hex(int x, int y, bool isBlack = false)
+            {
+                X = x;
+                Y = y;
+                IsBlack = isBlack;
+            }
+
+            public Hex E => new Hex(X + 10, Y);
+            public Hex SE => new Hex(X + 5, Y + 10);
+            public Hex SW => new Hex(X - 5, Y + 10);
+            public Hex W => new Hex(X - 10, Y);
+            public Hex NW => new Hex(X - 5, Y - 10);
+            public Hex NE => new Hex(X + 5, Y - 10);
+
+            public Hex GetNext(string direction)
+            {
+                switch (direction)
+                {
+                    case "e": return E;
+                    case "se": return SE;
+                    case "ne": return NE;
+                    case "w": return W;
+                    case "sw": return SW;
+                    case "nw": return NW;
+                }
+                throw new NotImplementedException();
+            }
+
+            public override bool Equals(object? obj)
+            {
+                return Equals(obj as Hex);
+            }
+
+            public override int GetHashCode()
+            {
+                return HashCode.Combine(X, Y);
+            }
+
+            public bool Equals(Hex other)
+            {
+                if (ReferenceEquals(other, this)) return true;
+                if (ReferenceEquals(other, null)) return false;
+
+                return X == other.X && Y == other.Y;
+            }
+
+            public void Mark()
+            {
+                IsBlack = !IsBlack;
+            }
+
+            public IEnumerable<Hex> GetAround()
+            {
+                yield return E;
+                yield return SE;
+                yield return NE;
+                yield return W;
+                yield return SW;
+                yield return NW;
+            }
+
+            public Hex MarkNew()
+            {
+                return new Hex(X, Y, !IsBlack);
+            }
+
+            public override string ToString()
+            {
+                return (IsBlack ? "B " : "W ") + ((double)X)/10 + "-" + ((double)Y)/10;
+            }
+        }
+
+        private List<List<string>> Directions = new List<List<string>>();
+        public void InitInput(string content)
+        {
+            var splitContent = content.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+
+            foreach (var currentLine in splitContent)
+            {
+                var resplacePart1 = currentLine
+                    .Replace("se", " se ")
+                    .Replace("sw", " sw ")
+                    .Replace("nw", " nw ")
+                    .Replace("ne", " ne ");
+                Directions.Add(resplacePart1
+                    .Replace("ew", " e w ")
+                    .Replace("we", " w e ")
+                    .Replace("ee", " e e ")
+                    .Replace("ww", " w w ")
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList());
+            }
+        }
+
+        public string SolveFirstProblem()
+        {
+            Hexes = new HashSet<Hex>();
+            foreach (var directionList in Directions)
+            {
+                var currentHex = new Hex(0, 0);
+                foreach (var direction in directionList)
+                {
+                    currentHex = currentHex.GetNext(direction);
+                }
+
+                if (Hexes.TryGetValue(currentHex, out var realCurrentHex))
+                {
+                    realCurrentHex.Mark();
+                }
+                else
+                {
+                    currentHex.Mark();
+                    Hexes.Add(currentHex);
+                }
+            }
+
+            return Hexes.Count(h => h.IsBlack).ToString();
+        }
+
+        HashSet<Hex> Hexes { get; set; }
+
+        public string SolveSecondProblem(string firstProblemSolution)
+        {
+            for (int i = 0; i < 100; i++)
+                TurnOneRound();
+
+            return Hexes.Count.ToString();
+        }
+
+        private void TurnOneRound()
+        {
+            HashSet<Hex> toTreat = new HashSet<Hex>();
+            HashSet<Hex> finalBlack = new HashSet<Hex>();
+
+            foreach (var hex in Hexes)
+            {
+                // Any black tile with zero or more than 2 black tiles immediately adjacent to it is flipped to white.
+                if (hex.IsBlack)
+                {
+                    int countBlackAround = 0;
+
+                    foreach (var aroundHex in hex.GetAround())
+                    {
+                        if (!toTreat.TryGetValue(aroundHex, out var toTreatAroundHex))
+                        {
+                            if (!Hexes.TryGetValue(aroundHex, out toTreatAroundHex))
+                                toTreatAroundHex = aroundHex;
+                        }
+
+                        if (toTreatAroundHex.IsBlack)
+                            countBlackAround++; // count for rule 1
+                        else
+                            toTreat.Add(toTreatAroundHex); // keep around hexes for rule 2
+                    }
+
+                    if (countBlackAround == 1 || countBlackAround == 2)
+                        finalBlack.Add(hex);
+                }
+            }
+
+            //Any white tile with exactly 2 black tiles immediately adjacent to it is flipped to black.
+            foreach (var hex in toTreat)
+            {
+                var blackAround = 0;
+                foreach (var around in hex.GetAround())
+                {
+                    if (Hexes.TryGetValue(around, out var maybeBlackAround) && maybeBlackAround.IsBlack)
+                        blackAround++;
+                }
+
+                if (blackAround == 2)
+                {
+                    finalBlack.Add(hex.MarkNew());
+                }
+            }
+
+            Hexes = finalBlack;
+        }
+
+        public bool Question2CodeIsDone { get; } = true;
+    }
+}
---
+++ AdventOfCode2020/Solvers/SolverDay25.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace AdventOfCode2020.Solvers
+{
+    class SolverDay25 : ISolver
+    {
+        private BigInteger publicKey1;
+        private BigInteger publicKey2;
+        public void InitInput(string content)
+        {
+            var splitContent = content.Split(new string[] {"\r\n"}, StringSplitOptions.None);
+
+            publicKey1 = BigInteger.Parse(splitContent[0]);
+            publicKey2 = BigInteger.Parse(splitContent[1]);
+        }
+
+        public BigInteger LoopOver(BigInteger sn, BigInteger value)
+        {
+            return (value * sn) % 20201227;
+        }
+
+        public int FindLoopValueFor(BigInteger finalValue, BigInteger sn)
+        {
+            BigInteger value = 1;
+            for (int i = 1; i > -1; i++)
+            {
+                value = LoopOver(sn, value);
+                if (value == finalValue)
+                    return i;
+            }
+
+            return -1;
+        }
+        public BigInteger CalculateLoop(BigInteger loopValue, BigInteger sn)
+        {
+            BigInteger current = 1;
+            for (int i = 0; i < loopValue; i++)
+            {
+                current = LoopOver(sn, current);
+            }
+
+            return current;
+        }
+
+        public string SolveFirstProblem()
+        {
+            var loopSize1 = FindLoopValueFor(publicKey1, 7);
+            var loopSize2 = FindLoopValueFor(publicKey2, 7);
+            var serial = CalculateLoop(loopSize1, publicKey2);
+            var serial2 = CalculateLoop(loopSize2, publicKey1);
+
+            if (serial != serial2)
+                throw new NotImplementedException();
+
+            return serial.ToString();
+        }
+
+        public string SolveSecondProblem(string firstProblemSolution)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Question2CodeIsDone { get; } = false;
+    }
+}
